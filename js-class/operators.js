501d16684df56f404e4e3832e572326f72f8b208
Agatha França <agathafranca@outlook.com>
2021-09-18 14:43:51 -0300

    Shows the return of the comparison between values with relational operators

---
+++ js-class/operators.js
@@ -0,0 +1,15 @@
+/* Comparison operators 
+    == Equal to in value
+    < Less than in value
+    > Greater than in value
+    <= Less than or equal to in value
+    >= Greater than or equal to in value
+    != Not equal to
+    === Equal value and same type
+    !== Not equal value and not same type
+*/
+
+console.log( 5 > 4 ) // true
+console.log( 5 < 4) // false
+console.log( 5 >= 4 ) // true
+console.log( 4 <= 4 ) // true
