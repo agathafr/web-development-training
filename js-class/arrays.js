4e10bbf89b803606c12e03154fabb50a6c96b196
Agatha França <agathafranca@outlook.com>
2021-09-18 17:28:19 -0300

    Groups objects into a single variable

---
+++ js-class/arrays.js
@@ -0,0 +1,19 @@
+const students = [
+    {
+        studentName: "Mayk",
+        grade: 9.8
+    },
+    {
+        studentName: "Diego",
+        grade: 10
+    },
+    {
+        studentName: "Valeska",
+        grade: 2
+    }
+]
+
+const studentNames = ["Mayk", "Diego", "Valeska"]
+console.log(students[1])
+
+const average = (students[0].grade + students[1].grade + students[2].grade) / 3
