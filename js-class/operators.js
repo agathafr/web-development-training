9ba26e7d1ce6dd0fb10640781ffa1fe4863dd7c4
Agatha França <agathafranca@outlook.com>
2021-09-18 15:15:44 -0300

    Explains how logical operators work

--- js-class/operators.js
+++ js-class/operators.js
@@ -1,22 +1,14 @@
-/* Comparison operators 
-    == Equal to in value
-    < Less than in value
-    > Greater than in value
-    <= Less than or equal to in value
-    >= Greater than or equal to in value
-    != Not equal to
-    === Equal value and same type
-    !== Not equal value and not same type
+/* Logical operators 
+    && Logical "and" evaluates to true when both operands are true
+    || Logical "or" evaluates to true when either operand is true
+    !  Logical "not" evaluates to true if the operand is false 
 */
 
-const age = 17
+console.log(5 == 5 && 6 == 6) // true
+console.log(5 == 5 && 6 != 6) // false
 
-if (age >= 18) {
-    console.log("Ages is greater than 18. You can come in!" )
-} else {
-    console.log("Locked entry")
-}
+console.log(5 == 5 || 6 == 6) // true
+console.log(5 == 5 || 6 != 6) // true
 
-if (age == 17) {
-    console.log("Come back when you turn 18.")
-}
+console.log(!(5 > 6)) // true
+console.log(!(5 < 6)) // false
