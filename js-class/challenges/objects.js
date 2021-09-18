06d7563757d6acbfaaa39d3bec181aa9d3dedbb6
Agatha França <agathafranca@outlook.com>
2021-09-18 17:46:15 -0300

    Create and print objects

---
+++ js-class/challenges/objects.js
@@ -0,0 +1,20 @@
+const company = {
+    companyName: "Rocketseat",
+    color: "Purple",
+    focus: "Programming",
+    address: {
+        street: "Street Guilherme Gembala",
+        number: 260
+    }
+}
+
+console.log(`The company ${company.companyName} is located at ${company.address.street}, ${company.address.number}`)
+
+const user = {
+    userName: "Diego",
+    company: {
+        companyName: "Rocketseat"
+    }
+}
+
+console.log(`The user name is ${user.userName}`)
