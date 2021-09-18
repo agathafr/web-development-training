3b6ee6146d4ee2832dff2cd66f407da0de1bdc9c
Agatha França <agathafranca@outlook.com>
2021-09-18 14:23:00 -0300

    Calculates the average between student grades

--- js-class/index.js
+++ js-class/index.js
@@ -1,4 +1,9 @@
-const nameOne = "Mayk"
-const nameTwo = "Diego"
-const nameThree = `Valeska e ${nameOne}`
-console.log(nameThree)
+const studentOne = 'Mayk'
+const studentTwo = 'Diego'
+const studentThree = 'Pedro'
+const gradeStudentOne = 9.8
+const gradeStudentTwo = 10
+const gradeStudentThree = 2
+
+const average = (gradeStudentOne + gradeStudentTwo + gradeStudentThree) / 3
+console.log(average)
