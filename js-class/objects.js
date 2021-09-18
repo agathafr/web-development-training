915def30b981c6680003123ecb1ccee4e1017c34
Agatha França <agathafranca@outlook.com>
2021-09-18 17:05:08 -0300

    Declares objects and accesses their properties

---
+++ js-class/objects.js
@@ -0,0 +1,16 @@
+const student01 = {
+    studentName: "Mayk",
+    grade: 9.8 
+}
+
+const student02 = {
+    studentName: "Diego",
+    grade: 10
+}
+
+const student03 = {
+    studentName: "Valeska",
+    grade: 2
+}
+
+const average = (student01.grade + student02.grade + student03.grade) / 3
