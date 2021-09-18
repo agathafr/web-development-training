f554042482776fbc453c44744545ce0d31c5b9c9
Agatha França <agathafranca@outlook.com>
2021-09-18 15:28:18 -0300

    Names and shows the use of arithmetic operators

--- js-class/operators.js
+++ js-class/operators.js
@@ -1,14 +1,15 @@
-/* Logical operators 
-    && Logical "and" evaluates to true when both operands are true
-    || Logical "or" evaluates to true when either operand is true
-    !  Logical "not" evaluates to true if the operand is false 
+/* Arithmetic operators 
+    + Addition
+    - Subtraction
+    * Multiplication
+    / Division
+    % Modulus 
+    ++ Before: Increment and then return value | After: Return value and then increment
+    -- Before: Decrement and then return value | After: Return value and then drecrement 
 */
 
-console.log(5 == 5 && 6 == 6) // true
-console.log(5 == 5 && 6 != 6) // false
-
-console.log(5 == 5 || 6 == 6) // true
-console.log(5 == 5 || 6 != 6) // true
-
-console.log(!(5 > 6)) // true
-console.log(!(5 < 6)) // false
+console.log(2 * 2) // 4
+console.log(2 / 2) // 1
+console.log(2 % 1.5) // 0.5
+console.log(2 + 2) // 4
+console.log(2 - 2) // 0
