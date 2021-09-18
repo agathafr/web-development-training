2dde5e2063c3724f4a400f2ce542d3cc3b4893ab
Agatha França <agathafranca@outlook.com>
2021-09-18 14:31:43 -0300

    Show a message in console according to a condition

--- js-class/index.js
+++ js-class/index.js
@@ -7,3 +7,9 @@ const gradeStudentThree = 2
 
 const average = (gradeStudentOne + gradeStudentTwo + gradeStudentThree) / 3
 console.log(average)
+
+if (average > 5){
+    console.log(`The average was of ${average.toFixed(2)}. Congratulations class!`)
+} else {
+    console.log(`The average was of ${average.toFixed(2)}. Study more class!`)
+}
