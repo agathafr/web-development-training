e8ed55cce6884f714df30962415ee258484e2d8c
Agatha França <agathafranca@outlook.com>
2021-09-18 16:27:01 -0300

    Created the logic of the BMI calculator challenge

---
+++ js-class/challenges/bmi.js
@@ -0,0 +1,13 @@
+const nameOfWhoWantsToKnowBMI = "Carlos";
+const weight = 84;
+const height = 1.88;
+
+const imc = weight / (height * height);
+
+if (imc >= 30) {
+    console.log(`${nameOfWhoWantsToKnowBMI}, you are overweight.`)
+} 
+
+if (imc < 29.9){
+    console.log(`${nameOfWhoWantsToKnowBMI}, you are not overweight.`)
+}
