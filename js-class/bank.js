0dd9388acff53ab23d95ef9c74d793c55036880b
Agatha França <agathafranca@outlook.com>
2021-09-19 14:23:47 -0300

    Adds transaction to an array of an object property

---
+++ js-class/bank.js
@@ -0,0 +1,23 @@
+const user = {
+    userName: "Mariana",
+    transactions: [],
+    balance: 0
+}
+
+const transaction = {
+    type: 'debit',
+    value: 50.5
+}
+
+function createTransaction(transact) {
+    user.transactions.push(transact)
+
+    if (transact.type == 'credit') {
+        return user.balance = transact.value + user.balance
+    } else {
+        return user.balance = user.balance - transact.value
+    }
+}
+
+createTransaction(transaction)
+console.log(user)
