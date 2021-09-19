4813110060be93ea2fb0e7bf068fce5ab0fb866b
Agatha França <agathafranca@outlook.com>
2021-09-19 12:44:06 -0300

    Indenting the code

--- js-class/challenges/programming.js
+++ js-class/challenges/programming.js
@@ -3,14 +3,14 @@ const object = {
     age: 19,
     technologies: [{
         technologyName: 'C++',
-        speciality: 'Desktop' 
-    },{
+        speciality: 'Desktop'
+    }, {
         technologyName: 'Python',
         speciality: 'Data Science'
-    },{
+    }, {
         technologyName: 'JavaScript',
         speciality: 'Web/Mobile'
-    }] 
+    }]
 }
 
 console.log(`The user ${object.programmerName} is ${object.age} years old and uses ${object.technologies[0].technologyName} technology with a specialty in ${object.technologies[0].speciality}`)
