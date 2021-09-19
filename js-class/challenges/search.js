8903c5a5b78412ddc8075efb2a6f7f60a21d91d8
Agatha França <agathafranca@outlook.com>
2021-09-19 12:48:51 -0300

    Indenting the code

--- js-class/challenges/search.js
+++ js-class/challenges/search.js
@@ -1,24 +1,24 @@
 const users = [{
     userName: "Carlos",
     technologies: ["HTML", "CSS"]
-},{
+}, {
     userName: "Jasmine",
     technologies: ["Javascript", "CSS"]
-},{
+}, {
     userName: "Tuane",
     technologies: ["HTML", "Node.js"]
 }]
 
 function checkIfTheUserUsesCSS(user) {
     for (let i = 0; i < user.technologies.length; i++) {
-        if (user.technologies[i] == "CSS"){
+        if (user.technologies[i] == "CSS") {
             return true
         }
     }
     return false
 }
 
-for (let i = 0; i < users.length; i++){
+for (let i = 0; i < users.length; i++) {
     const userWorksWithCSS = checkIfTheUserUsesCSS(users[i])
 
     if (userWorksWithCSS) {
