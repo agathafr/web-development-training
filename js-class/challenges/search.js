c0d1c987bafb08c381c95e34e2a99410f839ac7e
Agatha França <agathafranca@outlook.com>
2021-09-19 10:25:36 -0300

    Creates function that searches for CSS users

---
+++ js-class/challenges/search.js
@@ -0,0 +1,27 @@
+const users = [{
+    userName: "Carlos",
+    technologies: ["HTML", "CSS"]
+},{
+    userName: "Jasmine",
+    technologies: ["Javascript", "CSS"]
+},{
+    userName: "Tuane",
+    technologies: ["HTML", "Node.js"]
+}]
+
+function checkIfTheUserUsesCSS(user) {
+    for (let i = 0; i < user.technologies.length; i++) {
+        if (user.technologies[i] == "CSS"){
+            return true
+        }
+    }
+    return false
+}
+
+for (let i = 0; i < users.length; i++){
+    const userWorksWithCSS = checkIfTheUserUsesCSS(users[i])
+
+    if (userWorksWithCSS) {
+        console.log(`The user ${users[i].userName} works with CSS.`)
+    }
+}
