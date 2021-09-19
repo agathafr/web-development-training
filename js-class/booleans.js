83615adc7527226d2fb5cca886caf2f3e6f29a60
Agatha França <agathafranca@outlook.com>
2021-09-19 12:51:03 -0300

    Indenting the code

--- js-class/booleans.js
+++ js-class/booleans.js
@@ -9,7 +9,7 @@ const studentsOfClassA = [{
 {
     studentName: "Valeska",
     grade: 2
-}, 
+},
 {
     studentName: "Vitória",
     grade: 6
@@ -26,7 +26,7 @@ const studentsOfClassB = [{
 {
     studentName: "Ana",
     grade: 2
-}, 
+},
 {
     studentName: "Fabrício",
     grade: 5
@@ -34,19 +34,19 @@ const studentsOfClassB = [{
 
 function markAsFailed(student) {
     student.disapproved = false
-    if(student.grade < 5) {
+    if (student.grade < 5) {
         student.disapproved = true
     }
 }
 
 function sendMessageDisapproved(student) {
-    if (student.disapproved){
+    if (student.disapproved) {
         console.log(`The student ${student.studentName} is desapproved!`)
     }
 }
 
 function studentDisapproved(students) {
-    for(let student of students) {
+    for (let student of students) {
         markAsFailed(student)
         sendMessageDisapproved(student)
     }
