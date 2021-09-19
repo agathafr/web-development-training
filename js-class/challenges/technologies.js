5ac9a456893747fd2948db1af75c313cacd79751
Agatha França <agathafranca@outlook.com>
2021-09-19 12:49:39 -0300

    Indenting the code

--- js-class/challenges/technologies.js
+++ js-class/challenges/technologies.js
@@ -1,14 +1,14 @@
 const users = [{
     userName: "Carlos",
     technologies: ["HTML", "CSS"]
-},{
+}, {
     userName: "Jasmine",
     technologies: ["Javascript", "CSS"]
-},{
+}, {
     userName: "Tuane",
     technologies: ["HTML", "Node.js"]
 }]
 
-for (let user of users){
+for (let user of users) {
     console.log(`${user.userName} trabalha com ${user.technologies}`)
 }
